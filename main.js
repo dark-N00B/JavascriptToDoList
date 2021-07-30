c3f295dcf0e1277f31f0145a2f2b59f174ef0151
darkNOOB <darkn00b@protonmail.com>
2021-07-31 01:20:08 +0530

    taskDone tick and action fixed

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -18,15 +17,13 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('done');
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('taskDone');
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("taskInput").value;
